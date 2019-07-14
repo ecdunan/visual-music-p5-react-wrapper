138a832449ab1cfb1df074a3ab28dc675d55a723
ecdunan <dunan.emmanuel@gmail.com>
2019-07-15 00:06:19 +0800

    Added one time upload

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -10,6 +10,7 @@ class App extends React.Component {
 
         this.state = {
             stateSketch: sketch,
+            uploadedSong: null,
             volume: 0.5,
             isPlaying: false,
             buttonText: 'Play'
@@ -27,14 +28,20 @@ class App extends React.Component {
         });
     }
 
+    onFileUpload = event => {
+        this.setState({uploadedSong: event.target.files[0]});
+    }
+
     render() {
         return (
             <div>
-                <P5Wrapper sketch={this.state.stateSketch} volume={this.state.volume} isPlaying={this.state.isPlaying}/>
+                <P5Wrapper sketch={this.state.stateSketch} volume={this.state.volume} isPlaying={this.state.isPlaying} uploadedSong={this.state.uploadedSong}/>
                 <button onClick={this.onPlayPress}>{this.state.buttonText}</button>
                 <br/>
                 <label>Volume</label>
                 <input type="range" name="volume" value={this.state.volume}  min="0.0"  max="1.0" step="0.1" onChange={this.onVolumeChange}/>
+                <br/>
+                <input type="file" name="file" onChange={this.onFileUpload}/>
             </div>
         );
     }
--- src/vendor/sketches/sketch.js
+++ src/vendor/sketches/sketch.js
@@ -1,49 +1,49 @@
 import p5 from 'p5';
 import 'p5/lib/addons/p5.sound';
 import 'p5/lib/addons/p5.dom';
-import mist from './mist.mp3';
 
 export default function sketch(p) {
-    let song;
-    let amplitude;
-    let volume;
-    let isPlaying = false;
+  let song;
+  let amplitude;
+  let volume;
+  let isPlaying = false;
 
-    p.preload = function() {
-      song = p.loadSound(mist);
-    }
+  p.setup = function () {
+    p.createCanvas(200, 200);
+    amplitude = new p5.Amplitude();
+  }
 
-    p.setup = function () {
-      p.createCanvas(200, 200);
-      amplitude = new p5.Amplitude();
-    }
+  p.myCustomRedrawAccordingToNewPropsHandler = function (props) {
+    if(song) {
+      if(song.isLoaded()) {
+        volume = props.volume;
+        isPlaying = props.isPlaying;
 
-    p.myCustomRedrawAccordingToNewPropsHandler = function (props) {
-      volume = props.volume;
-      isPlaying = props.isPlaying;
-
-      if(song) {
+        song.setVolume(parseFloat(volume));
         p.togglePlaying(song);
       }
-   }
-
-    p.togglePlaying = function(song) {
-      if(isPlaying && !song.isPlaying()) {
-        song.play();
-      } else if (!isPlaying && song.isPlaying()) {
-        song.pause();
+    } else {
+      if(props.uploadedSong) {
+        song = p.loadSound(props.uploadedSong);
       }
     }
+  }
 
-    p.draw =function () {
-      p.background(51);
-
-      let songVolume = amplitude.getLevel();
-      let ellipseDiameter = p.map(songVolume, 0, 1, 10, 200); // you need the map() in order to get a big enough ellipse
-
-      song.setVolume(parseFloat(volume));
-
-      p.fill(255,0,255);
-      p.ellipse(p.width/2, p.height/2, ellipseDiameter, ellipseDiameter);
+  p.togglePlaying = function(song) {
+    if(isPlaying && !song.isPlaying()) {
+      song.play();
+    } else if (!isPlaying && song.isPlaying()) {
+      song.pause();
     }
+  }
+
+  p.draw =function () {
+    p.background(51);
+
+    let songVolume = amplitude.getLevel();
+    let ellipseDiameter = p.map(songVolume, 0, 1, 10, 200); // you need the map() in order to get a big enough ellipse
+
+    p.fill(255,0,255);
+    p.ellipse(p.width/2, p.height/2, ellipseDiameter, ellipseDiameter);
+  }
 };
